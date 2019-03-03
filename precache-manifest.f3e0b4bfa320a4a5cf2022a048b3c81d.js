fb15e1f3d34c910b96bd13c9dda973960d358ac0
nurainiaguse <aguse2@illinois.edu>
2019-03-03 11:55:29 -0600

    Updates

---
+++ precache-manifest.f3e0b4bfa320a4a5cf2022a048b3c81d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0e319f87483090a0303e",
+    "url": "/test-react/static/js/runtime~main.0e319f87.js"
+  },
+  {
+    "revision": "f5195fb5a7a0451107c5",
+    "url": "/test-react/static/js/main.f5195fb5.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/test-react/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "f5195fb5a7a0451107c5",
+    "url": "/test-react/static/css/main.6c252cba.chunk.css"
+  },
+  {
+    "revision": "6ffde262748c5d5710e7e067a3995eac",
+    "url": "/test-react/index.html"
+  }
+];
